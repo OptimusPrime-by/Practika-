7cb6e7b08e711f4970711fd1851dc5d1c8c25988
Optimys.by <73966301+OptimusPrime-by@users.noreply.github.com>
2022-08-15 22:51:16 +0300

    Add files via upload

---
+++ 11/11/11/11.cpp
@@ -0,0 +1,37 @@
+﻿#include "number_theory.h"
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    int m = 112, p = 1553, g = 17, x = 14, k = 117, rk = 0, s = 0, r = 0;
+    int y = DegreeRemainder1(g, x, p);
+    r = DegreeRemainder1(g, k, p);
+    if (NODEuclidean(k, p - 1) == 1) {
+        rk = InverseBezu(k, p - 1);
+    }
+    else {
+        cout << "Nod(k,p-1)≠1";
+        return -1;
+    }
+    if (m - x * r < 0) {
+        int H = m - x * r;
+        int j = -H / (p - 1) + 1;
+        int h = j * (p - 1) + H;
+        s = (rk * h) % (p - 1);
+    }
+    else s = (rk * m - x * r) % (p - 1);
+    cout << "Параметр y = " << y << endl << "Подпись (r,s) >> " << "(" << r << "," << s << ")" << endl;
+
+    int remainderY = DegreeRemainder1(y, r, p);
+    int remainderR = DegreeRemainder1(r, s, p);
+    int remainderG = DegreeRemainder1(g, m, p);
+
+    if (remainderG == (remainderY * remainderR) % p)cout << "Подпись действительна!";
+    else cout << "Подпись недействительна!";
+    return 0;
+}
